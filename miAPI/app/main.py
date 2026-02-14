24b71c859f189e007862116275edb8f4c81127a0
Santiago Meneses <sanmenesest17@gmail.com>
2026-02-13 20:34:05 -0600

    PUT terminado

---
+++ miAPI/app/main.py
@@ -0,0 +1,79 @@
+#Importaciones
+from fastapi import FastAPI, status, HTTPException
+import asyncio
+from typing import Optional
+
+
+#Instancia del servidor
+app = FastAPI(
+   title='Mi Primer API',
+   description='Santiago Meneses',
+   version='1.0.0'
+)
+
+#TB ficticia
+usuarios=[
+   {"id":1, "nombre":"Juan", "edad":21},
+   {"id":2, "nombre":"Israel", "edad":21},
+   {"id":3, "nombre":"Sofi", "edad":21}
+]
+
+
+@app.get("/", tags=['Inicio'])
+async def bienvenida():
+   return {"mensaje": "Bienvenido a mi API!"}
+
+@app.get("/HolaMundo", tags=['Bienvenida Asincrona'])
+async def hola():
+   await asyncio.sleep(4) #simulación de una peticion
+   return {
+      "mensaje": "Hola mundo FastAPI!",
+      "estatus": "200"
+      }
+
+@app.get("/v1/ParametroOB/{id}", tags=['Parametro Obligatorio'])
+async def consultaUno(id:int):
+   return {"Se encontro un usuario": id}
+
+@app.get("/v1/ParametroOP/", tags=['Parametro Opcional'])
+async def consultaTodos(id:Optional[int]=None):
+   if id is not None:
+      for usuario in usuarios:
+         if usuario["id"] == id:
+            return{"mensaje": "Usuario encontrado", "usuario":usuario}
+      return{"mensaje": "Usuario NO encontrado", "usuario":id}
+   else:
+      return{"mensaje": "No se proporciono id"}
+
+
+@app.get("/v1/usuarios/", tags=['CRUD HTTP'])
+async def leer_Usuarios():
+   return{
+      "status":"200",
+      "total": len(usuarios),
+      "usuarios":usuarios
+   }
+   
+@app.post("/v1/usuarios/", tags=['CRUD HTTP'], status_code=status.HTTP_201_CREATED)
+async def crear_Usuario(usuario:dict):
+   for usr in usuarios:
+      if usr["id"] == usuario.get("id"):
+         raise HTTPException(
+            status_code=400,
+            detail="El id ya existe"
+         )
+   usuarios.append(usuario)
+   return{
+      "mensaje":"Usuario Agregado",
+      "Usuario":usuario
+   }
+
+
+@app.put("/v1/usuario/{id}", tags=['Actualizar Usuario'])
+async def actualizar_Usuario(id: int, usuario_actualizado: dict):
+    for index, usr in enumerate(usuarios): 
+        if usr["id"] == id:
+            usuarios[index].update(usuario_actualizado)
+            return {"mensaje": "Usuario actualizado", "usuario": usuarios[index]}
+    
+    return {"mensaje": "Usuario no encontrado", "id": id}
