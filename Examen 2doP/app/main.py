2167f617228c6e7a81476c306b99a0618b05d2a7
Santiago Meneses <sanmenesest17@gmail.com>
2026-03-10 20:30:48 -0600

    CommitFinal

--- Examen 2doP/app/main.py
+++ Examen 2doP/app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI, status, HTTPException, Depends
-import asyncio
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel, Field
-from typing import Literal, List, Optional
-from datetime import datetime
+from typing import Literal, List
 import secrets
 
 app = FastAPI(
@@ -13,15 +11,15 @@ app = FastAPI(
 )
 
 tickets = [
-   {"id":1, "usuario":"Juan", "descripcion":"La computadora no enciende", "prioridad":"alta", "estado":"pendiente"},
-   {"id":2, "usuario":"Israel", "descripcion":"La impresora no responde al comando de impresión.", "prioridad":"media", "estado":"resuelto"},
-   {"id":3, "usuario":"Sofi", "descripcion":"El internet está muy lento.", "prioridad":"baja", "estado":"pendiente"}
+   {"ticket_id":1, "usuario":"Juan Gutierrez", "descripcion":"La computadora no enciende", "prioridad":"alta", "estado":"pendiente"},
+   {"ticket_id":2, "usuario":"Israel Gomez", "descripcion":"La impresora no responde al comando de impresión.", "prioridad":"media", "estado":"resuelto"},
+   {"ticket_id":3, "usuario":"Sofia Alvarez", "descripcion":"El internet está muy lento.", "prioridad":"baja", "estado":"pendiente"}
 ]
 
 class Ticket(BaseModel):
-    id: int
+    ticket_id: int
     usuario: str = Field(..., min_length=5, example="Santiago Meneses")
-    description: str = Field(..., min_length=20, max_length=200, example="La impresora no responde al comando de impresión.")
+    descripcion: str = Field(..., min_length=20, max_length=200, example="La impresora no responde al comando de impresión.")
     prioridad: Literal['baja', 'media', 'alta'] = Field(..., example="media") 
     estado: Literal['pendiente', 'resuelto'] = Field(..., example="pendiente")
     
@@ -39,10 +37,10 @@ def verificar_Peticion(credenciales: HTTPBasicCredentials=Depends(security)):
 
    return credenciales.username
 
-@app.post("/tickets", status_code=status.HTTP_201_CREATED, response_model=Ticket)
+@app.post("/v1/Tickets/", status_code=status.HTTP_201_CREATED)
 async def crear_ticket(ticket: Ticket):
       for tckt in tickets:
-         if tckt["id"] == ticket.id:
+         if tckt["id"] == ticket.ticket_id:
             raise HTTPException(
                status_code=400,
                detail="El id ya existe"
@@ -52,7 +50,7 @@ async def crear_ticket(ticket: Ticket):
       "mensaje":"Ticket Agregado",
    }
 
-@app.get("/tickets", response_model=List[Ticket])
+@app.get("/v1/Tickets/", status_code=status.HTTP_200_OK)
 async def obtener_tickets():
    return{
       "status":"200",
@@ -60,11 +58,42 @@ async def obtener_tickets():
       "tickets":tickets
       }
 
-@app.get("/tickets/{ticket_id}", response_model=Ticket, status_code=status.HTTP_200_OK, )
+@app.get("/v1/Tickets/{ticket_id}", status_code=status.HTTP_200_OK, )
 async def obtener_ticket(ticket_id: int, userAuth: str = Depends(verificar_Peticion)):
    for tckt in tickets:
       if tckt["id"] == ticket_id:
          return tckt
+   raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail="Ticket no encontrado"
+   )
+
+@app.put("/v1/Tickets/estado/{ticket_id}", status_code=status.HTTP_200_OK)
+async def cambiar_estado_ticket(ticket_id: int, ticket: Ticket, userAuth: str = Depends(verificar_Peticion)):
+   for i, tckt in enumerate(tickets):
+      if tckt["id"] == ticket_id:
+         tickets[i] = ticket.model_dump()
+         return ticket
+   raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail="Ticket no encontrado"
+   )
+
+
+@app.delete("/v1/Tickets/{ticket_id}", status_code=status.HTTP_200_OK)
+async def eliminar_ticket(ticket_id: int):
+   for i, tckt in enumerate(tickets):
+      if tckt["id"] == ticket_id:
+         if tckt["estado"] == "resuelto":
+            raise HTTPException(
+               status_code=status.HTTP_400_BAD_REQUEST,
+               detail="Solo se pueden eliminar tickets pendientes"
+            )
+         
+         del tickets[i]
+         return{
+            "mensaje":"Ticket Eliminado"
+         }
    raise HTTPException(
       status_code=status.HTTP_404_NOT_FOUND,
       detail="Ticket no encontrado"
