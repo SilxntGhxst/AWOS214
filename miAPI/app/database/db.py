0dcf249215972b0f9805d5fdf7f1ffedb07ebd93
Santiago Meneses <sanmenesest17@gmail.com>
2026-03-18 19:16:15 -0600

    Practica 9 (parte 1) terminada

---
+++ miAPI/app/database/db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+#1. Definimos la URL de la BD
+DATABASE_URL = os.getenv(
+   "DATABASE_URL",
+   "postgresql://admin:123456@postgres:5434/DB_miapi"
+)
+
+#2. Creamos el motor de la BD
+engine = create_engine(DATABASE_URL)
+
+#3. Creamos el gestionador de sesiones
+SessionLocal = sessionmaker(
+   autocommit=False, 
+   autoflush=False, 
+   bind= engine
+)
